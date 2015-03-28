3422f85d275793d7faf9545afe1be953e8a29f9b
ivanmijatovic89 <ivanmijatovic89@gmail.com>
2015-03-28 04:59:52 +0100

    views

--- src/ivanmijatovic89/ProtoViewGenerator/routes.php
+++ src/ivanmijatovic89/ProtoViewGenerator/routes.php
@@ -1,18 +1,12 @@
 <?php
 
-
-// Route::get('protomaker', function(){
-// 	echo "PROTO MAKER";
-// });
-
- // Route::get('protomaker', '\ivanmijatovic89\ProtoViewGenerator\ProtoMakerController@index');
-
 Route::group(['namespace' => '\ivanmijatovic89\ProtoViewGenerator\Controller'], function(){
 
          Route::get('protomaker', ['as'=>'protomaker.index','uses'=>'ProtoMakerController@index']);
+
          Route::get('protomaker-documentation', ['as'=>'protomaker.docs','uses'=>'ProtoMakerController@docs']);
 
          Route::post('protoMakeCrud', ['as'=>'protoMakeCrud','uses'=> 'ProtoMakerController@protoMakeCrud']);
-    // ovo je test
+
 });
 
